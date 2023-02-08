0355d1cb7ea6f3eb1e28f5e37f53206379cc9412
18137036 <9wjdgh9@naver.com>
2023-02-08 22:55:40 +0900

    Feat : TodoHead component isSidebar state 생성

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,6 +1,7 @@
 
 import styled from 'styled-components';
 import { HiMenu } from "react-icons/hi";
+import { useState } from 'react';
 
 const TodoHeadBlock = styled.div`
     padding: 16px 32px;
@@ -22,14 +23,28 @@ const HeadTitle = styled.h1`
     flex-grow: 2;
     margin-left : 20px;
 `
+
+const IconContainer = styled.div`
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+`
 function TodoHead(props) {
+    const [isSidebar, setisSidebar] = useState(false);
+
+    const showSidevbar  = () => {
+        console.log(isSidebar)
+        setisSidebar(!isSidebar)
+    }
+    
     return (
         <header>
             <TodoHeadBlock>
                 <LogoIcon src="../images/logo.png" alt="강아지 발바닥 로고"/>
                 <HeadTitle>멍두리스트</HeadTitle>
-                <HiMenu size={25}/>
-
+                <IconContainer onClick={showSidevbar}>
+                    <HiMenu size={25}/>
+                </IconContainer>
             </TodoHeadBlock>
         </header>
     );
