368de58b5c027a9359d2c423146ae3317d641067
18137036 <9wjdgh9@naver.com>
2023-02-08 17:41:32 +0900

    Feat : Todoitem component html,css 구현

---
+++ src/components/Todoitem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const TodoListBlock = styled.div`
+    
+`
+function Todoitem(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Todoitem;
