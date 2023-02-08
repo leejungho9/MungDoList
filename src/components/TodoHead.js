c054d236a02ce1a63c257c4c44c9f348e7b1af9f
18137036 <9wjdgh9@naver.com>
2023-02-08 18:04:06 +0900

    Feat : font 추가 및 TodoHead font 적용

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -3,33 +3,31 @@ import styled from 'styled-components';
 import { HiMenu } from "react-icons/hi";
 
 const TodoHeadBlock = styled.div`
-
-padding: 16px 32px;
-border-bottom: 1px solid #dfdfdf;
-display: flex;
-align-items: center;
-justify-content: space-between;
-background-color:  #FFFFF9;
-
-img {
+    padding: 16px 32px;
+    border-bottom: 1px solid #dfdfdf;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color:  #FFFFF9;
+`
+const LogoIcon = styled.img`
     width: 20px;
     height: 20px;
-}
-
-h1{
+`
+const HeadTitle = styled.h1`
+    font-family: "GowunDodum";
     flex-basis: 150px;
     font-size: 20px;
     letter-spacing: 5px;
     flex-grow: 2;
     margin-left : 20px;
-}
 `
 function TodoHead(props) {
     return (
         <header>
             <TodoHeadBlock>
-                <img src="../images/logo.png" alt="강아지 발바닥 로고"></img>
-                <h1>멍두리스트</h1>
+                <LogoIcon src="../images/logo.png" alt="강아지 발바닥 로고"/>
+                <HeadTitle>멍두리스트</HeadTitle>
                 <HiMenu size={25}/>
 
             </TodoHeadBlock>
