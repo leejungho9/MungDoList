aa2c088d5e1a6a90244b6f418e0c748ab3355a82
18137036 <9wjdgh9@naver.com>
2023-02-08 17:50:18 +0900

    Feat : TodoList component html, js 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TodoListBlock = styled.div`
+
+
+`
+
 
 function TodoList(props) {
+
+    const date = new Date();
+    const days = ['일','월','화','수','목','금','토'];
+
     return (
-        <div>
-            
-        </div>
+        <main>
+            <TodoListBlock>
+                <div className='day'>{`${days[date.getDay()]}요일`}</div>
+            </TodoListBlock>
+        </main>
     );
 }
 
