74b35414aa135e2779a04c1914562b3e7e1cfee7
18137036 <9wjdgh9@naver.com>
2023-02-12 19:08:33 +0900

    Feat : redux 세팅

--- src/commons/actions.js
+++ src/commons/actions.js
@@ -1,15 +1,16 @@
-export const ADD = "ADD_TODO";
-export const DELETE = "DELETE_TODO";
-export const UPDATE = "UPDATE_TODO";
-export const TOGGLE = "TOGGLE_TODO";
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const GET_TODO = "GET_TODO";
 
-let id = 5;
+
 
 export const addTodo = (todo) => {
   return {
-    type : ADD,
+    type : ADD_TODO,
     payload : {
-      id : id++,
+      id : todo.id,
       title : todo.title,
       describe : todo.describe,
       isCompleted: todo.isCompleted,
@@ -20,30 +21,35 @@ export const addTodo = (todo) => {
 
 export const deleteTodo = (id) => {
   return {
-    type : DELETE,
-    payload : {
-      id
-    }
+    type : DELETE_TODO,
+    payload : {id}
   }
 }
 
 export const updateTodo = (todo) => {
   return {
-    type : UPDATE,
+    type : UPDATE_TODO,
     payload : {
       id : todo.id,
-      title : todo.title,
-      isCompleted: todo.isCompleted,
-      date : todo.date
+      title : todo.title
     }
   }
 }
 
-export const toggleTodo = (id) => {
+export const toggleTodo = (todo) => {
+ 
   return {
-    type : TOGGLE,
+    type : TOGGLE_TODO,
     payload : {
-      id
+      id : todo.id,
+      isCompleted : todo.isCompleted
     }
+  }
+}
+
+export const getAllTodos = (todos) => {
+  return {
+    type : GET_TODO,
+    payload : {todos}
   }
 }
--- src/commons/reducer.js
+++ src/commons/reducer.js
@@ -1,71 +1,41 @@
 
-import { ADD, DELETE, UPDATE, TOGGLE } from "./actions"
+import { act } from "react-dom/test-utils"
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO, TOGGLE_TODO, GET_TODO } from "./actions"
+
 
 const initialState = {
-  todos : [
-    {
-      "id": 1,
-      "title": "아침 사료 주기",
-      "description": "오늘 쩡이랑 호수공원으로 산책을 다녀왔다. 쩡이가 추워할까봐 걱정을 많이 했는데 생각보다 잘 놀아서 기분이 너무 좋았다.",
-      "isCompleted": true,
-      "date": "2023-02-09"
-    },
-    {
-      "id": 2,
-      "title": "저녁 사료 주기",
-      "description": "오늘 쩡이랑 호수공원으로 산책을 다녀왔다. 쩡이가 추워할까봐 걱정을 많이 했는데 생각보다 잘 놀아서 기분이 너무 좋았다.",
-      "isCompleted": false,
-      "date": "2023-02-09"
-    },
-    {
-      "id": 3,
-      "title": "산책 30분 하기",
-      "description": "오늘 쩡이랑 호수공원으로 산책을 다녀왔다. 쩡이가 추워할까봐 걱정을 많이 했는데 생각보다 잘 놀아서 기분이 너무 좋았다.",
-      "isCompleted": false,
-      "date": "2023-02-09"
-    },
-    {
-      "id": 4,
-      "title": "기관지 영양제 먹이기",
-      "description": "오늘 쩡이랑 호수공원으로 산책을 다녀왔다. 쩡이가 추워할까봐 걱정을 많이 했는데 생각보다 잘 놀아서 기분이 너무 좋았다.",
-      "isCompleted": false,
-      "date": "2023-02-10"
-    },
-    {
-      "id": 5,
-      "title": "기관지 영양제 먹이기",
-      "description": "오늘 쩡이랑 호수공원으로 산책을 다녀왔다. 쩡이가 추워할까봐 걱정을 많이 했는데 생각보다 잘 놀아서 기분이 너무 좋았다.",
-      "isCompleted": false,
-      "date": "2023-02-11"
-    },
-  ]
+  todos : [ ]
 }
-
-
 export const reducer = (state = initialState, action) => {
-  if(action.type === ADD){
+  if(action.type === ADD_TODO){
     console.log(state.todos)
     return {
       todos: [...state.todos, action.payload],
     }
   }
 
-  if(action.type === DELETE){
+  if(action.type === DELETE_TODO){
     return {
       todos:  [...state.todos.filter((todo) => todo.id !== action.payload.id),] 
     }
   }
 
-  if(action.type === UPDATE){
+  if(action.type === UPDATE_TODO){
     return {
       todos : [...state.todos.map((todo) =>  todo.id === action.payload.id ? {...todo, title : action.payload.title} : todo)]
     }
   }
 
-  
-  if(action.type === TOGGLE){
+  if(action.type === TOGGLE_TODO){
+
     return {
-      todos : [...state.todos.map((todo) => todo.id === action.payload.id ? {...todo, isCompleted : !todo.isCompleted}: todo)]
+      todos : [...state.todos.map((todo) => todo.id === action.payload.id ? {...todo, isCompleted : action.payload.isCompleted}: todo)]
+    }
+  }
+
+  if(action.type === GET_TODO){
+    return {
+      todos : action.payload.todos
     }
   }
 
