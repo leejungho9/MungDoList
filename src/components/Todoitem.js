58a9002f53ed33adced4372007cd98c149cd6c34
18137036 <9wjdgh9@naver.com>
2023-02-12 18:47:56 +0900

    Feat : TodoUpdate, CheckToggle, TodoDelete 기능 구현

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,117 +1,168 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
-import { RiHeartAddLine } from "react-icons/ri";
-import { RxPencil2, RxTrash } from "react-icons/rx"
+import { RxPencil2 } from "react-icons/rx";
 import { TfiTrash } from "react-icons/tfi";
-import TodoCreate from "./TodoCreate";
 import { useDispatch } from "react-redux";
-import { deleteTodo } from "../commons/actions";
-const TodoItemBlock = styled.div`
-  padding: 50px 32px;
-`;
+import { deleteTodo, toggleTodo, updateTodo } from "../commons/actions";
+import { useEffect } from "react";
+import axios from "axios";
+import { computeHeadingLevel } from "@testing-library/react";
 
-const TodoItemContainer = styled.div`
-  height: 510px;
-`;
+const TodoItemBlock = styled.div``;
+
 const TodoListBox = styled.div`
   padding: 10px 0;
   display: flex;
   align-items: center;
 
-  div{
+  div {
     display: none;
   }
 
-  &:hover{
+  &:hover {
     div {
       display: block;
-      .icons{
+      .icons {
         margin: 0 5px;
       }
     }
   }
 `;
+
 const TodoCheckbox = styled.input.attrs({ type: "checkbox" })`
   width: 18px;
   height: 18px;
   cursor: pointer;
 `;
-const TodoLabel = styled.label`
+const TodoInput = styled.input.attrs({ type: "text" })`
   margin-left: 30px;
   font-size: 20px;
   font-family: "Gowun Dodum";
   cursor: pointer;
   flex-grow: 2;
-  text-decoration: ${(props) => props.checked ? "line-through" : "none"};
+  border: none;
+  text-decoration: ${props => props.checked && "line-through"};
+
+  &:focus {
+    outline: none;
+  }
   &:hover {
     font-weight: bold;
   }
 `;
 
 const TodoHoverBox = styled.div`
-  .update:hover{
+  .update:hover {
     transform: scale(1.1);
     cursor: pointer;
   }
-  .delete:hover{
+  .delete:hover {
     transform: scale(1.1);
     cursor: pointer;
-  }
-`
-
-const AddTodoItem = styled.div`
-  width: 45px;
-  height: 45px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid #d1d1d1;
-  background-color: #ffffff;
-  cursor: pointer;
-  float: right;
-  transition: all 0.2s linear;
-  .addIcon {
-    color: #eb7e2f;
-  }
-  &:hover {
-    transform: scale(1.1);
   }
 `;
 
-
-function Todoitem({ todayTodos }) {
+function Todoitem({ todoItem }) {
   const dispatch = useDispatch();
-  const [showInput, setShowInput ]  = useState(false);
-  const checkboxChange = (e) => {
-    console.log(e.target.checked)
+  // 수정모드 on/off
+  const [editMode, setEditMode] = useState(false);
+  // 수정할 Input
+  const [newText, setNewText] = useState(todoItem.title);
+  // input DOM 접근
+  const TodoInputRef = useRef();
+
+  // checkbox 선택
+  const checkboxChange =  (e, id) => {
+    handleCheck(id);
+  };
+
+  const handleCheck = async (id) => {
+
+    const todo = {
+      id: id,
+      isCompleted : !todoItem.isCompleted
+    };
+
+    const response = await axios.patch(`http://localhost:4000/todos/${id}`, todo);
+    dispatch(toggleTodo(response.data));
   }
-  const handleDelete = (id) => {
-    console.log(id)
-    dispatch(deleteTodo(id))
-  }
+
+  // Todo 삭제하기
+  const handleDelete = async (id) => {
+    const response = await axios.delete(`http://localhost:4000/todos/${id}`);
+    console.log(response);
+    window.location.reload();
+  };
+
+  // input text onChange 이벤트
+  const handleChange = (e) => {
+    setNewText(e.target.value);
+  };
+
+  // enter 눌렀을 때 이벤트
+  const handleKeyPress = (e, id) => {
+    if (e.key === "Enter") {
+      handleClick(id);
+    }
+  };
+
+  // Todo 수정하기
+  const handleClick = async (id) => {
+    const todo = {
+      id: id,
+      title: newText,
+    };
+    console.log(todo);
+    const response = await axios.patch(
+      `http://localhost:4000/todos/${id}`,
+      todo
+    );
+
+    dispatch(updateTodo(response.data));
+    setEditMode(false);
+  };
+
+
+  //edit 모드일때 input 포커싱
+  useEffect(() => {
+    if (editMode) {
+      TodoInputRef.current.focus();
+    } else {
+      TodoInputRef.current.blur();
+    }
+  }, [editMode]);
+
+
+  console.log(todoItem)
+
   return (
-    <TodoItemBlock>
-      <TodoItemContainer>
-        {todayTodos.map((todo) => {
-          return (
-            <TodoListBox key={todo.id}>
-              <TodoCheckbox id={todo.id} checked={todo.isCompleted} onChange={checkboxChange}/>
-              <TodoLabel htmlFor={todo.id} checked={todo.isCompleted}>{todo.title}</TodoLabel>
-              <TodoHoverBox>
-                <RxPencil2 size={20} className="icons update" />
-                <TfiTrash size={20} className="icons delete" onClick={() => handleDelete(todo.id)}/>
-              </TodoHoverBox>
-            </TodoListBox>
-          );
-        })}
-        <TodoListBox>
-          {showInput && <TodoCreate showInput={showInput} setShowInput={setShowInput}/>}
-        </TodoListBox>
-      </TodoItemContainer>
-      <AddTodoItem onClick={() => setShowInput(!showInput)}>
-        <RiHeartAddLine className="addIcon" size={30}/>
-      </AddTodoItem>
+    <TodoItemBlock key={todoItem.id}>
+      <TodoListBox>
+        <TodoCheckbox
+          checked={todoItem.isCompleted}
+          onChange={(e) => checkboxChange(e, todoItem.id)}
+        />
+        <TodoInput
+          ref={TodoInputRef}
+          checked={todoItem.isCompleted}
+          value={newText}
+          readOnly={!editMode}
+          onChange={handleChange}
+          onKeyDown={(e) => handleKeyPress(e, todoItem.id)}
+        ></TodoInput>
+        <TodoHoverBox>
+          <RxPencil2
+            size={20}
+            className="icons update"
+            onClick={() => setEditMode(!editMode)}
+          />
+          <TfiTrash
+            size={20}
+            className="icons delete"
+            onClick={() => handleDelete(todoItem.id)}
+          />
+        </TodoHoverBox>
+      </TodoListBox>
     </TodoItemBlock>
   );
 }
