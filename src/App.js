3d9a696a61e79aff90cb7b6668e244ea52f21ee7
18137036 <9wjdgh9@naver.com>
2023-02-12 19:01:52 +0900

    Feat : json-server 에서 todo data 받아오기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Route, useLocation } from "react-router-dom";
 import "./App.css";
-import TodoContainer from "./components/TodoContainer";
-import TodoHead from "./components/TodoHead";
-import TodoMainList from "./components/TodoMainList";
+import { getAllTodos } from "./commons/actions";
 import Main from "./pages/Main";
+
 function App() {
+ const dispatch = useDispatch();
+
+  //json-server 에서 todo data 받아오기
+  useEffect(() => {
+   async function getTodos() {
+     try {
+      const response = await axios.get("http://localhost:4000/todos");
+      console.log(response.data)
+      dispatch(getAllTodos(response.data))
+     } catch (err) {
+       console.log(err)
+     }
+   }
+   getTodos();
+ }, [dispatch]);
+
 
   return (
     <BrowserRouter>
-      <Route path="/" component={Main} />
-      {/* <Route path="/list" component={TodoList} /> */}
+      <Route path="/" component={Main}   />
     </BrowserRouter>
   );
 }
